35f0ce3b25d6d2bca06b9b6572be955653a116ea
broknBRAIN <priyanshyadav99399@gmail.com>
2025-07-02 17:03:05 +0530

    middleware logging

--- server.js
+++ server.js
@@ -13,6 +13,20 @@ app.use(bodyParser.json()); //stored the data in req.body as javaScript object
 
 const port = 3232;
 
+//middleware logging
+//middle ware is a which runs between a request -> middleware ->response
+//mostly used for logging
+const logRequest = (req,res,next) => {
+    console.log(` [${new Date().toLocaleString()}] Request made to : ${req.originalUrl} `);
+    //prints the time when url is hit
+    //logging
+    next();
+    //after logging just process the next command it had to do(middleware is done)
+};
+
+//to use middleware in the whole project
+app.use(logRequest);
+
 app.get('/', (req, res) => {
   res.send('you have getted')
 });
